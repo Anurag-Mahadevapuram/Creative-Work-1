b2ec38c04678d51180392a62d0dc30ac01228ffb
manurag <manurag@uco.edu>
2022-01-22 18:46:38 -0600

    39. routing

--- public/viewpage/about_page.js
+++ public/viewpage/about_page.js
@@ -1,12 +1,14 @@
 import * as Elements from './elements.js'
+import { routePath } from '../controller/route.js';
 
 export function addEventListeners(){
     Elements.menuAbout.addEventListener('click', ()=>{
+        history.pushState(null, null, routePath.ABOUT);
         about_page();
     });
 }
 
-function about_page(){
+export function about_page(){
     Elements.root.innerHTML = `
     <h1>About Page</h1>
     `;
--- public/viewpage/home_page.js
+++ public/viewpage/home_page.js
@@ -1,12 +1,15 @@
 import * as Elements from './elements.js'
+import { routePath } from '../controller/route.js';
+
 export function addEventListeners(){
 
     Elements.menuHome.addEventListener('click', ()=>{
+        history.pushState(null, null, routePath.HOME);
         home_page();
     });
 }
 
-function home_page(){
+export function home_page(){
     Elements.root.innerHTML = `
         <h1>Home Page</h1>
     `;
